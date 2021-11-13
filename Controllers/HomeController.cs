6c3c6b9e155368df51d79f1f41da64acbc9ea479
Eduardo Quirino <edu.3532@gmail.com>
2021-11-13 20:27:54 -0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC__BO.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC__BO.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        //Get
+        public IActionResult Index()
+        {
+            AlunoBLL _aluno = new AlunoBLL();
+
+            List<Aluno> alunos = _aluno.GetAlunos().ToList();
+
+            return View("Lista",alunos);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //Post
+        [HttpPost]
+        public IActionResult Create(Aluno aluno)
+        {
+           
+            //if (aluno.Nascimento <= DateTime.Now.AddYears(-18)) 
+            //{
+            //    ModelState.AddModelError("Nascimento", "data de nascimento invalida...");
+            //} if (String.IsNullOrEmpty(aluno.Nome))
+            //    ModelState.AddModelError("Nome", "O nome é obrigatório");
+
+            //if (String.IsNullOrEmpty(aluno.Sexo))
+            //    ModelState.AddModelError("Sexo", "O sexo é obrigatório");
+
+            //if (String.IsNullOrEmpty(aluno.Email))
+             
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                AlunoBLL _aluno = new AlunoBLL();
+                _aluno.IncluirAluno(aluno);
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC__BO.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O nome seve ser informado!")]
+        [StringLength(50,MinimumLength =5, ErrorMessage ="O nome deve ter no minimo 5 caracters!" )]
+        [Display(Name ="Informe o Nome do cliente!")]
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "O campo sexo deve ser informado!")]
+        [Display(Name = "Informe o Sexo do cliente!")]
+        public string Sexo { get; set; }
+
+
+        [Required(ErrorMessage ="O email dever ser informado!")]
+        [EmailAddress(ErrorMessage ="Email invalido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A data de nascimento deve ser informada!")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+        public DateTime Nascimento { get; set; }
+    }
+}
